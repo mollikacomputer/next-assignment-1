bc53197e035f3b0260e7b07a28c75714d13ffc07
mollikacomputer <mollikacomputer3@gmail.com>
2026-05-07 18:27:37 +0600

    answer to the first question

--- src/solutions.ts
+++ src/solutions.ts
@@ -1,1 +1,10 @@
-// problem 1
+// problem 1 answer
+const filterEvenNumbers = (numbers: number[]): number[] => {
+  const result = numbers.filter((num) => num % 2 === 0);
+  return result;
+};
+// check result 1 way
+const evenNumber = filterEvenNumbers([101, 100, 105, 1010, 112, 113, 114, 116]);
+console.log(evenNumber);
+// another way to get result
+// console.log(filterEvenNumbers([101, 100, 105, 1010, 112, 113, 114, 116]));
